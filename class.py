c5fe8c3b950330cc99c0d228d969c2b77ae7003e
medo2progcoding <107712819+medo2progcoding@users.noreply.github.com>
2022-07-20 01:57:43 +0200

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+print("Enter a password : ")
+user_password = input("")
+
+if len(user_password) > 8 : #or len(user_password) >  :
+    print("you must enter a password from 8 digits : ")
+
+elif len(user_password) <= 15 : #or len(user_password) >  :
+    print("you must enter a password less than 16 digits : ")
+
+#if len(user_password) <= 8 and len(user_password) > 16 :
+   # print("print")
+  
+
